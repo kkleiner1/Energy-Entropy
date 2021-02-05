d46aad2fa220c20a65a315d45f3f483b826389a6
yuanshunyue <yuan@LAPTOP-F27JOTCA.localdomain>
2021-02-04 18:14:52 -0600

    Update functions gathering data and calculating entropies

--- gather.py
+++ gather.py
@@ -6,88 +6,132 @@ import itertools
 import pandas as pd
 import glob 
 
-def read_RHF(fname):
-    with h5py.File(fname,'r') as f:
-       # print(list(f['scf'].keys()))
-        e_tot = f['scf']['e_tot'][()]
-        return e_tot
+def separate_variables_in_fname(spl):
+    method=spl[0]
+    startingwf=spl[1]
+    i=1
+    if "hci" in startingwf:
+        i+=1
+    orbitals=spl[i+1]
+    statenumber=spl[i+2]
+    nconfig=spl[i+3]
+    if "dmc" in method:
+        method+=spl[i+4]
+    return method,startingwf,orbitals,statenumber,nconfig
 
 def extract_from_fname(fname):
-    is_opt="True"
-    spl = fname.split('_')
-    bond_length=float(spl[3])
-    basis = spl[5].replace(".chkfile","")
-    if 'orbitalopt' in spl:
-        is_opt = spl[7].replace(".chkfile","")
-        
-    print(is_opt)   
-    return {"basis":basis,
-            "bond length": bond_length,"orbital_optimize":is_opt}
+    fname=fname.replace('.chk','')
+    spl=fname.split('/')
+    if '_' in spl[3]:
+        spl_2=spl[3].split('_')
+        method,startingwf,orbitals,statenumber,nconfig=separate_variables_in_fname(spl_2)
+        if (startingwf=="mf"):
+            startingwf=spl[1]
+    else: 
+        startingwf=spl[1]
+        orbitals,nconfig="/","/"
+        statenumber=0
+        method=spl[3]
+        if (method=="mf"):
+            method=spl[1]
+
+    return {"bond_length": spl[0][3:],
+            "basis":spl[2],
+            "startingwf":startingwf,
+            "method":method,
+            "orbitals":orbitals,
+            "statenumber":statenumber,
+            "determinant_cutoff":0,
+            "nconfig":nconfig
+            }
 
 
-def create_RHF(fname):
-    e_tot = read_RHF(fname)
-    record = extract_from_fname(fname)
-    record.update({'method':'RHF',
-           "energy":e_tot,
-           "error":0.0,
-    })
-    return record
+def avg(data):
+    mean=np.mean(data,axis=0)
+    error=np.std(data,axis=0)/np.sqrt(len(data)-1)
+    return mean,error
 
-def read_ccsd(filename):
-    with h5py.File(filename) as f:
-        return f['ccsd']['energy'][()],list(f['ccsd']['rdm'])
+def calculate_entropy(dm):
+    if len(dm.shape) == 2:
+        dm = np.asarray([dm/2.0,dm/2.0])
+    u,v = np.linalg.eig(dm)
+    #print(u)
+    u = u[u>0]
+    return -np.sum(np.log(u)*u).real
 
-def create_ccsd(fname):
-    record=extract_from_fname(fname)
-    e_tot = read_ccsd(fname)[0]
-    record.update({'method':'CCSD',
-           "energy":e_tot,
-           "error":0.0
-    })
-    return record
-
-
-def avg(vec):
-    nblock = vec.shape[0]
-    avg = np.mean(vec,axis=0)
-    std = np.std(vec,axis=0)
-    return avg, std/np.sqrt(nblock)
+def normalize_rdm(rdm1_value,rdm1_norm,warmup):
+    rdm1, rdm1_err=avg(rdm1_value[warmup:,...])
+    rdm1_norm, rdm1_norm_err = avg(rdm1_norm[warmup:,...])
+    rdm1=pyqmc.obdm.normalize_obdm(rdm1,rdm1_norm)
+    rdm1_err=pyqmc.obdm.normalize_obdm(rdm1_err,rdm1_norm) 
+    return rdm1,rdm1_err
     
 def read_vmc(fname):
     with h5py.File(fname,'r') as f:
-        en = f['energytotal'][1:]
-        nblocks=len(en)
-
-        enavg = np.mean(en)
-        enerr=stats.sem(en)
-        
+        #print(list(f.keys()))
         warmup=2
-        en_vec = f['energytotal'][warmup:,...]
-        en, en_err = avg(en_vec)
-        rdm1, rdm1_err=avg(f['rdm1value'][warmup:,...])
-        rdm1norm, rdm1norm_err = avg(f['rdm1norm'][warmup:,...])
-        rdm1=pyqmc.obdm.normalize_obdm(rdm1,rdm1norm)
-        rdm1_err=pyqmc.obdm.normalize_obdm(rdm1_err,rdm1norm)
+        energy=f['energytotal'][warmup:,...]
+        e_tot,error=avg(energy)
+    
+        rdm1_up,rdm1_up_err=normalize_rdm(f['rdm1_upvalue'],f['rdm1_upnorm'],warmup)
+        rdm1_down,rdm1_down_err=normalize_rdm(f['rdm1_downvalue'],f['rdm1_downnorm'],warmup)
+        rdm1=np.array([rdm1_up,rdm1_down])
+        entropy=calculate_entropy(rdm1)
+        return e_tot,error,entropy
 
-        return enavg,enerr,rdm1,rdm1_err
+def read_dmc(fname):
+    return read_vmc(fname)
 
+def read(fname):
+    with h5py.File(fname,'r') as f:
+        e_tot,error,entropy=0.0,0.0,0.0
+        method=extract_from_fname(fname)["method"]
+        if 'cc' in method:
+            e_tot=f['ccsd']['energy'][()]
+        elif 'vmc' in method:
+            e_tot,error,entropy=read_vmc(fname)
+        elif 'dmc' in method:
+            e_tot,error,entropy=read_dmc(fname)
+        elif 'hf' in method: 
+            e_tot = f['scf']['e_tot'][()]
+        elif 'fci' in method:
+            e_tot=np.array(f['e_tot'][()])[0] #state0,1,2,3,
+        elif 'hci' in method:
+            e_tot=np.array(f['ci']['energy'])[0] #state0,1,2,3
+        return e_tot,error,entropy
 
-def create_vmc(fname):
-    record=extract_from_fname(fname)
-    e_tot, err = read_vmc(fname)[0:2]
-    record.update({'method':'VMC',
+def create(fname):
+    e_tot,error,entropy = read(fname)
+    record = extract_from_fname(fname)
+    record.update({
            "energy":e_tot,
-           "error":err,
+           "error":error,
+           "entropy":entropy
     })
     return record
 
-
-
 if __name__=="__main__":
-    df_rhf = pd.DataFrame([create_RHF(fname) for fname in glob.glob("RHF*")])
-    df_ccsd = pd.DataFrame([create_ccsd(fname) for fname in glob.glob("ccsd*")])
-    df_vmc = pd.DataFrame([create_vmc(fname) for fname in glob.glob("vmc*.chkfile")])
-    df = pd.concat([df_rhf, df_ccsd, df_vmc])
-    df.to_csv("energies.csv", index=False)
+    fname=[]
+    fqmc=[]
+    fhci=[]
+    for name in glob.glob('**/*.chk',recursive=True):
+        method=extract_from_fname(name)["method"]
+        startingwf=extract_from_fname(name)["startingwf"]
+        if 'opt' in method:
+            continue
+        if ('hci' in method or 'hci' in startingwf):
+            fhci.append(name)
+        if ('vmc' in method or 'dmc' in method):
+            fqmc.append(name)
+        fname.append(name)
+
+    df=pd.DataFrame([create(name) for name in fname])
+    df.to_csv("h2_data.csv", index=False)
+
+    df1=pd.DataFrame([create(name) for name in fqmc])
+    df1 = df1[(df1.basis=='vtz')]
+    df1.to_csv("h2_qmc.csv", index=False)
+
+    #df2=pd.DataFrame([create(name) for name in fhci])
+    #df2.to_csv("hci.csv", index=False)
     
--- run_workflow.py
+++ run_workflow.py
@@ -1,2 +1,39 @@
 import snakemake
-snakemake.snakemake("Snakefile",cores=1,targets=["h2_1.4/hf/vtz/mf.chk"]) 
+import numpy as np
+import itertools
+
+molecule = 'h2'
+basis = ['vtz','vqz']
+bond_lengths = [1.2,1.4,1.6,1.8,2.0,3.0,4.0,4.4,4.8,5.0,5.2]
+
+prod = itertools.product(basis,bond_lengths)
+
+cc_target,vmc_target,dmc_target = [],[],[]
+fci_target,vmc_hci,dmc_hci = [],[],[]
+
+for basis,length in prod:
+    #hf_target.append(f"{molecule}_{length}/hf/{basis}/mf.chk")
+    #uhf_target.append(f"{molecule}_{length}/uhf/{basis}/mf.chk")
+    cc_target.append(f"{molecule}_{length}/hf/{basis}/mf.chk")
+    fci_target.append(f"{molecule}_{length}/uhf/{basis}/fci.chk")
+
+    determinant_cutoff=0
+    tstep=0.02
+    for tol in [0.1]: #,0.08,0.05,0.02
+        #hci_target.append(f"{molecule}_{length}/hf/{basis}/hci{tol}.chk")
+        vmc_hci.append(f"{molecule}_{length}/hf/{basis}/vmc_hci{tol}_{determinant_cutoff}_fixed_0_400.chk")
+        vmc_hci.append(f"{molecule}_{length}/hf/{basis}/vmc_hci{tol}_{determinant_cutoff}_orbitals_0_400.chk")
+        dmc_hci.append(f"{molecule}_{length}/hf/{basis}/dmc_hci{tol}_{determinant_cutoff}_fixed_0_400_{tstep}.chk")
+        dmc_hci.append(f"{molecule}_{length}/hf/{basis}/dmc_hci{tol}_{determinant_cutoff}_orbitals_0_400_{tstep}.chk")
+
+    dmc_target.append(f"{molecule}_{length}/hf/{basis}/dmc_mf_fixed_0_400_{tstep}.chk")
+    dmc_target.append(f"{molecule}_{length}/hf/{basis}/dmc_mf_orbitals_0_400_{tstep}.chk")
+    vmc_target.append(f"{molecule}_{length}/hf/{basis}/vmc_mf_orbitals_0_400.chk")  #actually nconfig=8000 in rules
+    vmc_target.append(f"{molecule}_{length}/hf/{basis}/vmc_mf_fixed_0_400.chk")
+    #vmc_target.append(f"{molecule}_{length}/uhf/{basis}/vmc_mf_orbitals_0_400.chk")
+    #vmc_target.append(f"{molecule}_{length}/uhf/{basis}/vmc_mf_fixed_0_400.chk")
+
+targets = cc_target+vmc_target+dmc_target+fci_target+vmc_hci+dmc_hci
+files = " ".join(targets)
+print(targets)
+snakemake.snakemake("Snakefile",cores=1,targets=targets,forcetargets=True)
